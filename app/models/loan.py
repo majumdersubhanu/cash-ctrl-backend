1c34205048b01fd3425814bf9187994c4e577342
Subhanu Majumder <reach.subhanu@gmail.com>
2026-02-25 08:44:00 +0530

    feat(models): implement loan.py part 3

--- app/models/loan.py
+++ app/models/loan.py
@@ -13,3 +13,18 @@ from app.utils.enums import LoanStatus, LoanRepaymentFrequency
 if TYPE_CHECKING:
     from app.models.user import User
     from app.models.contact import Contact
+    from app.models.account import Account
+
+
+class Loan(Base, UUIDMixin, TimestampMixin):
+    __tablename__ = "loans"
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id")
+    )  # Extender of loan
+    contact_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("contacts.id"))
+
+    # Whether the user is LENDING money to the contact, or BORROWING from them
+    is_lending: Mapped[bool] = mapped_column(default=True)
+
+    amount: Mapped[Decimal] = mapped_column(Numeric(12, 2))
