2a101da79a3145d71b9af923723e1547c9676a90
Subhanu Majumder <reach.subhanu@gmail.com>
2026-02-25 20:42:00 +0530

    feat(services): implement account_service.py part 3

--- app/services/account_service.py
+++ app/services/account_service.py
@@ -13,3 +13,18 @@ class AccountService:
     async def create_account(
         self,
         db: AsyncSession,
+        user_id: uuid.UUID,
+        name: str,
+        account_type: str,
+        initial_balance: float = 0,
+        currency: str = "USD"
+    ) -> Account:
+        account = Account(
+            name=name, type=account_type, balance=initial_balance, user_id=user_id, currency=currency
+        )
+
+        return await self.repo.create(db, account)
+
+    async def get_accounts(self, db: AsyncSession, user_id: uuid.UUID):
+        return await self.repo.get_user_accounts(db, user_id)
+
