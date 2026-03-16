27d55ffb285b8bb7664b0a1415067fbf47f0a33c
Subhanu Majumder <reach.subhanu@gmail.com>
2026-03-16 23:34:11 +0530

    feat(api): configure ACID-compliant database and documentation routes

--- app/settings.py
+++ app/settings.py
@@ -88,11 +88,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL')
 }
+DATABASES['default']['ATOMIC_REQUESTS'] = True  # Ensure ACID compliance by default
 
 
 # Password validation
